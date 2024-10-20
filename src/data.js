088c6579efbdb745fe2ffa25ab9aaf66b7cd7cd0
carolinaluna24 <143300268+carolinaluna24@users.noreply.github.com>
2024-10-20 00:28:26 -0500

    Ajustes para que en el deploy en vercel se muestren las imagenes

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ export const projects = [
     title: "Chat GPT to PDF",
     subtitle: "Tecnología Node.js",
     description: "Aplicación que permite generar un chatbot que responda dudas a partir del contenido de un PDF usando la API de OpenAI.",
+    //image: "https://github.com/carolinaluna24/my-portfolio/blob/main/src/assets/chatbot.512x470.png",
     image: "./src/assets/chatbot.512x470.png",
     link: "https://github.com/carolinaluna24/ChatGptPDF"
   },
